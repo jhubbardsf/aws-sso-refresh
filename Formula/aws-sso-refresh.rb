79c85c244427f8e1776fa7511d4eb1f1951e3434
Josh Hubbard <josh@joshuahubbard.dev>
2025-12-25 00:06:19 -0500

    fix(formula): add SHA256 hash

--- Formula/aws-sso-refresh.rb
+++ Formula/aws-sso-refresh.rb
@@ -2,7 +2,7 @@ class AwsSsoRefresh < Formula
   desc "Automatically refresh AWS SSO sessions before they expire"
   homepage "https://github.com/jhubbardsf/aws-sso-refresh"
   url "https://github.com/jhubbardsf/aws-sso-refresh/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # Will be filled in after first release
+  sha256 "ddabc79716df7c6fab8bcdf6ab44b066e68e1b64abbb12eb3e65422abc299dc7"
   license "MIT"
   head "https://github.com/jhubbardsf/aws-sso-refresh.git", branch: "main"
 
